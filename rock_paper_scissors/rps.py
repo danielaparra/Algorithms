3472aa2787d83ec8493582e437677c8780df5114
Daniela Parra <dyparra47@gmail.com>
2019-02-21 18:47:06 -0500

    Not finished rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,45 @@
 
 import sys
 
+options = ['rock', 'paper', 'scissors']
+
+def recurse(n, arr, option):
+  
+  newArr = arr
+  if n == 0:
+    newArr = [[]] if arr == [] else arr
+  else:
+    if newArr == []:
+      newArr.append([option])
+      return recurse(n-1, arr, option)
+    else:
+      print(arr)
+      print(n)
+      for item in arr:
+        for option in options:
+          newItem = item
+          newItem.append(option)
+          newArr.append(newItem)
+          print(newItem)
+        newArr.remove(item)
+      return recurse(n-1, newArr, option)
+    
+    #check for repeats
+  print(newArr)
+  return newArr
+
 def rock_paper_scissors(n):
-  pass 
+  permutations = []
+  options = ['rock', 'paper', 'scissors']
+
+  
+
+  for option in options:
+    permutation_for_option = recurse(n,[],option)
+    permutations.append(*permutation_for_option)
+
+  print(permutations)
+  return permutations
 
 
 if __name__ == "__main__":
